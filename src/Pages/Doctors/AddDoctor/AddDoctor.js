2326f99464a8826ba97c6cc1121f00d813a98dcb
Bayu Sri Hernogo <srihernogo@gmail.com>
2023-03-20 14:07:02 +0700

    Add select item in add doctors page

--- src/Pages/Doctors/AddDoctor/AddDoctor.js
+++ src/Pages/Doctors/AddDoctor/AddDoctor.js
@@ -8,12 +8,50 @@ import {
   RadioGroup,
   TextField,
   Typography,
+  Select,
+  MenuItem,
+  OutlinedInput,
+  InputLabel,
 } from '@mui/material';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// ListItem
+import { useTheme } from '@mui/material/styles';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = ['MBBS', 'BCS', 'FCPS', 'PHD', 'BMBS', 'MBChC', 'MBBCh'];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 const AddDoctor = () => {
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
   return (
     <Box
       style={{
@@ -103,17 +141,51 @@ const AddDoctor = () => {
             />
           </Grid>
           <Grid item xs={12} md={4}>
-            <Typography variant="OVERLINE TEXT">Degree</Typography>
+            <Typography variant="OVERLINE TEXT">Choose Degrees</Typography>
           </Grid>
           <Grid item xs={12} md={8} sx={{ marginLeft: { md: '-5rem' } }}>
-            <TextField
+            <Box>
+              <Select
+                labelId="demo-multiple-chip-label"
+                id="demo-multiple-chip"
+                multiple
+                value={personName}
+                onChange={handleChange}
+                variant="standard"
+                // rows={5}
+                fullWidth
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                renderValue={(selected) => (
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+                MenuProps={MenuProps}
+              >
+                <MenuItem disabled value="">
+                  <em>You Can Choose Multiple Degrees </em>
+                </MenuItem>
+                {names.map((name) => (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    style={getStyles(name, personName, theme)}
+                  >
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Box>
+            {/* <TextField
               id="standard-basic"
               label="Choose Degree"
               variant="standard"
               multiline
               rows={5}
               fullWidth
-            />
+            /> */}
           </Grid>
           <Grid item xs={12} md={4}>
             <Typography variant="OVERLINE TEXT">Salary</Typography>
