77201620101d1816049cd9a2ace0988d10da8de9
Bayu Sri Hernogo <srihernogo@gmail.com>
2023-03-20 13:31:30 +0700

    Fix treeView in doctors

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -26,6 +26,7 @@ import { Container } from '@mui/material';
 import { BsCardChecklist } from 'react-icons/bs';
 import TreeView from '@mui/lab/TreeView';
 import TreeItem from '@mui/lab/TreeItem';
+export { useState } from 'react';
 
 const drawerWidth = 240;
 
@@ -152,25 +153,39 @@ export default function NewHeader() {
               </ListItemButton>
             </ListItem>
           </NavLink>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FaUserNurse style={{ color: '#000', fontSize: '1.5rem' }} />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
           <TreeView
-            style={{ color: '#000', textAlign: 'start', padding: '0 1rem' }}
+            style={{
+              color: '#000',
+              background: '#fff',
+              textAlign: 'justify',
+              paddingLeft: '2.5rem',
+              marginTop: '-2.4rem',
+            }}
             aria-label="file system navigator"
+            defaultExpanded={['1']}
             defaultCollapseIcon={
-              <div style={{ padding: '.8rem 0' }}>
+              <div style={{ padding: '.2rem 0', visibility: 'hidden' }}>
                 <FaUserNurse style={{ color: '#000', fontSize: '1.5rem' }} />
               </div>
             }
             defaultExpandIcon={
-              <div style={{ padding: '.8rem 0' }}>
+              <div style={{ padding: '.2rem 0', visibility: 'hidden' }}>
                 <FaUserNurse style={{ color: '#000', fontSize: '1.5rem' }} />
               </div>
             }
-            sx={{ height: 160, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+            sx={{ height: 155, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
           >
             <TreeItem
               nodeId="1"
               label="Doctors"
-              style={{ color: '#000', PaddingLeft: '2rem' }}
+              style={{ color: '#000', background: '#FFFFFF' }}
             >
               <NavLink
                 to="/doctors"
@@ -183,7 +198,7 @@ export default function NewHeader() {
                         style={{ color: '#000', fontSize: '1.5rem' }}
                       />
                     </ListItemIcon>
-                    <ListItemText primary="Doctors List" />
+                    <ListItemText primary="Doctor List" />
                   </ListItemButton>
                 </ListItem>
               </NavLink>
