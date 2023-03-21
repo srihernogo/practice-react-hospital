a1f86853373152f1174c97c4841e12be9a6f50af
Bayu Sri Hernogo <srihernogo@gmail.com>
2023-03-21 14:14:20 +0700

    Update form added

--- src/Pages/Doctors/UpdateDoctor/UpdateDoctor.js
+++ src/Pages/Doctors/UpdateDoctor/UpdateDoctor.js
@@ -1,3 +1,6 @@
+import { ElevatorSharp } from '@mui/icons-material';
+import { Button, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -6,7 +9,6 @@ const UpdateDoctor = () => {
   const [searchTerm, setSearchTerm] = useState(''); // value of the search input
   const [doctors, setDoctors] = useState([]); // list of doctors
   const [filteredNames, setFilteredNames] = useState([]); // list of names that match the search term
-  console.log(doctors);
 
   //   search doctor by name
 
@@ -18,17 +20,50 @@ const UpdateDoctor = () => {
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     const formData = new FormData(e.target);
+  //     const name = formData.get("name");
+  //     const phone = formData.get("phone");
+  //     const address = formData.get("address");
+  //     const data = {
+  //       name,
+  //       phone,
+  //       address,
+  //     }; // picture
+  //     handleUpdate(data);
+  //   };
+  const handleUpdate = (id) => {
+    fetch(`http://localhost:5000/doctors/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        specialist: 'Cardiologist',
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          alert('Updated Successfully');
+        }
+      });
+  };
 
   const handleSearch = () => {
-    const filteredNames = doctors.filter((doctor) => {
-      doctor.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    setFilteredNames(filteredNames);
-    console.log(filteredNames);
+    if (searchTerm === '') {
+      return alert('Please enter a name to search');
+    } else {
+      const filteredNames = doctors.filter((doctor) => {
+        return doctor.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      setFilteredNames(filteredNames);
+    }
   };
 
   return (
-    <div style={{ height: '75vh' }}>
+    <div style={{ height: '100%' }}>
       {/* search box */}
       <input
         type="text"
@@ -45,10 +80,154 @@ const UpdateDoctor = () => {
       />
       <button onClick={handleSearch}>Search</button>
       <ul>
-        {filteredNames.map((name) => (
-          <div>
-            <li key={name}>{name.name}</li>
-            <li key={name}>{name._id}</li>
+        {filteredNames.map((doctor) => (
+          <div key={doctor._id}>
+            <form
+              style={{ background: '#fff', borderRadius: '5px' }}
+              onSubmit={handleUpdate}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Name
+                </Typography>
+                <TextField
+                  id="Name"
+                  name="name"
+                  variant="standard"
+                  value={doctor.name}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5 }}>
+                  Age
+                </Typography>
+                <TextField
+                  id="Name"
+                  name="age"
+                  variant="standard"
+                  value={doctor.age}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Address
+                </Typography>
+                <TextField
+                  id="address"
+                  name="address"
+                  variant="standard"
+                  value={doctor.address}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Email
+                </Typography>
+                <TextField id="Name" variant="standard" value={doctor.email} />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Phone
+                </Typography>
+                <TextField id="Name" variant="standard" value={doctor.phone} />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Degrees
+                </Typography>
+                <TextField
+                  id="Name"
+                  variant="standard"
+                  value={doctor.degrees}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Salary
+                </Typography>
+                <TextField id="Name" variant="standard" value={doctor.salary} />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Specialist
+                </Typography>
+                <TextField
+                  id="Name"
+                  variant="standard"
+                  value={doctor.specialist}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ mr: 5, mt: 2 }}>
+                  Time
+                </Typography>
+                <TextField id="Name" variant="standard" value={doctor.time} />
+              </Box>
+              {/* update button */}
+              <Button
+                variant="contained"
+                sx={{ mt: 2, mb: 2, ml: 2 }}
+                onClick={() => {
+                  handleUpdate(doctor._id);
+                }}
+              >
+                Update
+              </Button>
+            </form>
           </div>
         ))}
       </ul>
